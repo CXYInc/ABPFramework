f02ca6d2911bb7bb052430740c6d79f3b29b2699
cxyzgl <zhangguilin@cx580.com>
2018-12-14 17:59:32 +0800

    增加获取用户角色列表

--- src/CXY.CJS.Application/User/IUserServices.cs
+++ src/CXY.CJS.Application/User/IUserServices.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Application.Services;
+﻿using System.Collections.Generic;
+using Abp.Application.Services;
 using Abp.Application.Services.Dto;
 using CXY.CJS.Application.Dtos;
 using CXY.CJS.Model;
@@ -56,5 +57,14 @@ namespace CXY.CJS.Application
         /// <param name="id"></param>
         /// <returns></returns>
         Task<ApiResult<LowerAgentOutputItem>> GetLowerAgent(string id);
+
+
+        /// <summary>
+        /// 获取用户所拥有的角色
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<ApiResult<IEnumerable<UserRoleOutputItem>>> GetUserRoles(string id);
+        
     }
 }
---
+++ test/CXY.CJS.Tests/TestDatas/UserRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CXY.CJS.Tests.TestDatas
+{
+    public class UserRoles
+    {
+        public static readonly Model.UserRole LowerAgentTestRole = new Model.UserRole
+        {
+            Id =Guid.NewGuid().ToString(),
+            CreationTime = DateTime.Now,
+            RoleId = Guid.NewGuid().ToString(),
+            UserId = UserDatas.SuperWebSiteLowerAgent.Id
+        };
+    }
+}
