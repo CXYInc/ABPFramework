69ac047c309d5592b25acb110ec8ea5afa1778a9
洛林 李 <597760488@qq.com>
2018-12-18 16:41:32 +0800

    批次表

---
+++ src/CXY.CJS.Model/BatchCar.cs
@@ -0,0 +1,52 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CXY.CJS.Model
+{
+    public class BatchCar : Entity<string>, IHasCreationTime
+    {
+        public BatchCar()
+        {
+            CreationTime = Clock.Now;
+            IsNeedSearch = true;
+            PrivateCar = false;
+        }
+        public string Id { get; set; }
+        public string WebSiteId { get; set; }
+        /// <summary>
+        /// 车牌号
+        /// </summary>
+        public string CarNumber { get; set; }
+        /// <summary>
+        /// 车架号
+        /// </summary>
+        public string CarCode { get; set; }
+        /// <summary>
+        /// 发动机号
+        /// </summary>
+        public string EngineNo { get; set; }
+
+        /// <summary>
+        /// 是否个人车
+        /// </summary>
+        public bool PrivateCar { get; set; }
+        public string CarType { get; set; }
+        public string CarTypeName { get; set; }
+        public string IsLock { get; set; }
+        public string DriverName { get; set; }
+        public string DriverPhone { get; set; }
+        public string DriverLicense { get; set; }
+        public bool IsNeedSearch { get; set; }
+
+        public bool HaveLockRule { get; set; }
+
+        public bool IsChoose { get; set; }
+
+        public string ViolationMsg { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ src/CXY.CJS.Model/BatchViolation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CXY.CJS.Model
+{
+    class BatchViolation
+    {
+    }
+}
