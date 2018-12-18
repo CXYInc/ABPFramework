9796bfeee86e4d22bcc781bec89bc7403967c8eb
洛林 李 <597760488@qq.com>
2018-12-18 11:39:35 +0800

    角色授权

---
+++ src/CXY.CJS.Application/Role/Dto/GrantRoleMenuInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CXY.CJS.Application.Dtos
+{
+    public class GrantRoleMenuInput
+    {
+        [Required]
+        public string RoleId { get; set; }
+
+        [Required]
+        public string MenuId { get; set; }
+
+        [Required]
+        public bool IsGrant { get; set; }
+        //[Required]
+        //public List<ListMenu> ListMenu { get; set; }
+
+    }
+    //public class ListMenu
+    //{
+    //    public string MenuId { get; set; }
+    //    public bool IsGrant { get; set; }
+    //}
+}
