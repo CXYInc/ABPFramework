7848ed2aca2f805d5b455185f89286e1f3aaea78
洛林 李 <597760488@qq.com>
2018-12-20 18:47:13 +0800

    报价

--- src/CXY.CJS.Application/Price/PriceApplicationService.cs
+++ src/CXY.CJS.Application/Price/PriceApplicationService.cs
@@ -1,7 +1,11 @@
-﻿using Abp.Json;
+﻿using Abp.Domain.Repositories;
+using Abp.Json;
+using Abp.Runtime.Caching;
 using CXY.CJS.Application.Dtos;
 using CXY.CJS.Core.Config;
 using CXY.CJS.Core.HttpClient;
+using CXY.CJS.Core.WebApi;
+using CXY.CJS.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System;
@@ -16,11 +20,17 @@ namespace CXY.CJS.Application
     {
         private readonly ApiUrlConfig _apiConfig;
         private readonly HttpClientHelper _httpClientHelper;
+        private readonly IRepository<BatchCar, string> _batchCarRepository;
+        private readonly IRepository<BatchInfo, string> _batchInfoRepository;
+        private readonly ICacheManager _cacheManager;
 
-        public PriceAppService(IOptionsSnapshot<ApiUrlConfig> apiConfig, HttpClientHelper httpClientHelper)
+        public PriceAppService(IOptionsSnapshot<ApiUrlConfig> apiConfig, HttpClientHelper httpClientHelper, ICacheManager cacheManager, IRepository<BatchCar, string> batchCarRepository, IRepository<BatchInfo, string> batchInfoRepository)
         {
             _apiConfig = apiConfig.Value;
             _httpClientHelper = httpClientHelper;
+            _batchCarRepository = batchCarRepository;
+            _batchInfoRepository = batchInfoRepository;
+            _cacheManager = cacheManager;
         }
         [HttpPost]
         public async Task<Hashtable> IndoorPrice(IndoorPriceInput inputDto)
@@ -62,8 +72,9 @@ namespace CXY.CJS.Application
 
 
         [HttpPost]
-        public async Task<Hashtable> IndoorPriceBatch(IndoorPriceInput inputDto)
+        public async Task<ApiResult<List<PriceResultOutput>>> IndoorPriceBatch(IndoorPriceInput inputDto)
         {
+            ApiResult<List<PriceResultOutput>> apiResult = new ApiResult<List<PriceResultOutput>>();
             if (inputDto == null) return null;
 
             Hashtable postData = new Hashtable();
@@ -98,7 +109,54 @@ namespace CXY.CJS.Application
 
             var httpClientResponse = await _httpClientHelper.PostStringAsync(httpClientRequest);
 
-            return httpClientResponse.Data.FromJsonString<Hashtable>();
+            var priceList = httpClientResponse.Data.FromJsonString<List<PriceResultOutput>>();
+            return apiResult.Success(priceList);
+        }
+
+
+
+        public async Task<ApiResult> QuotePrice(PriceInput input)
+        {
+            ApiResult apiResult = new ApiResult();
+            Hashtable hst = new Hashtable();
+            if (!Limit(hst, input.BatchId, "QueryViolation_Agent", out string globalKey))
+            {
+                return apiResult.Error();
+            }
+            var batchInfo = await _batchInfoRepository.GetAsync(input.BatchId);
+            var carList = await _batchCarRepository.GetAllListAsync(o => o.WebSiteId == AbpSession.WebSiteId && o.BatchId == input.BatchId);
+
+            Hashtable data = new Hashtable
+            {
+                ["id"] = input.BatchId,
+                ["allCount"] = carList.Count,
+                ["completeCount"] = 0,
+                ["SearchSuccessCount"] = 0,
+                ["searchMessage"] = null,
+                ["successCount"] = 0
+            };
+            await _cacheManager.GetCache(globalKey).SetAsync(globalKey, data);
+
+
+
+            return apiResult.Success();
+        }
+
+        private bool Limit(Hashtable hst, string batchid, string business, out string globalKey)
+        {
+            globalKey = batchid + "." + business;
+
+            Hashtable data1 = _cacheManager.GetCache(globalKey) as Hashtable;
+            if (!object.Equals(data1, null))
+            {
+                hst["code"] = 2;
+                hst["message"] = "你上次的查询的任务还未执行完,请等任务执行完再开始下一次任务！";
+                hst["errormsg"] = "你上次的查询的任务还未执行完,请等任务执行完再开始下一次任务！";
+
+                hst["data"] = data1;
+                return false;
+            }
+            return true;
         }
     }
 }
