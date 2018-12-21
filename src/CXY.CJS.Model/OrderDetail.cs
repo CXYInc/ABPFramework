3c65090e69367ed170c0360f6de14c59e7b98950
洛林 李 <597760488@qq.com>
2018-12-21 16:43:44 +0800

    订单实体

--- src/CXY.CJS.Model/OrderDetail.cs
+++ src/CXY.CJS.Model/OrderDetail.cs
@@ -8,25 +8,8 @@ namespace CXY.CJS.Model
     public class OrderDetail : Entity<string>
     {
         public string WebSiteId { get; set; }
-        public int State { get; set; }
-        public string Apply { get; set; }
-        public string Applytime { get; set; }
-        public string PassId { get; set; }
 
-        /// <summary>
-        /// 是否核销
-        /// </summary>
-        public int IsDestory { get; set; }
-        public string DestoryRemark { get; set; }
-        public string PassMemo { get; set; }
-        public int ExportNum { get; set; }
-        public string CreatorUserId { get; set; }
-        public DateTime CreationTime { get; set; }
-        public string LastModifierUserId { get; set; }
-        public DateTime? LastModificationTime { get; set; }
-        public string DeleterUserId { get; set; }
-        public DateTime? DeletionTime { get; set; }
-        public bool IsDeleted { get; set; }
+      
 
     }
 }
